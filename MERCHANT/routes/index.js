114b3d9600973be1d44827b9ca15f6994cecd405
Alvin Januar <alvinjanuar234@ymail.com>
2019-08-31 17:36:29 +0700

    modified:   MERCHANT/routes/index.js 	modified:   MERCHANT/views/dashboard.ejs

--- MERCHANT/routes/index.js
+++ MERCHANT/routes/index.js
@@ -12,27 +12,4 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   })
 );
 
-router.post('/trade', (req, res) => {
-  const { currency,amount } = req.body;
-  let errors = [];
-  User.findOneAndUpdate(
-    {
-      user : req.user // search query
-    }, 
-    {
-      wallet: [{currency : "NTD",amount : 32}]   // field:values to update
-    },
-    {
-      new: true,                       // return updated doc
-      runValidators: true              // validate before update
-    })
-  .then(doc => {
-    console.log(doc)
-  })
-  .catch(err => {
-    console.error(err)
-  })
-   res.redirect('/dashboard');
-});
-
 module.exports = router;
